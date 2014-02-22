f7da5fb929927f0fe5809d0ee2ed480b4d7dafd1
Yohann Lepage <yohann@lepage.info>
2014-02-22 16:50:39 +0100

    Fixes for the packaging

--- setup.py
+++ setup.py
@@ -1,51 +1,48 @@
 # -*- coding: utf-8 -*-
 import os
-import re
+import sys
+
+import bitcasa
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-PACKAGE_NAME = 'bitcasa'
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-INIT = open(os.path.join(HERE, PACKAGE_NAME, '__init__.py')).read()
-README = open(os.path.join(HERE, 'README.md')).read()
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
-VERSION = re.search("__version__ = '([^']+)'", INIT).group(1)
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE.txt') as f:
-    license = f.read()
+with open('README.rst') as f:
+    README = f.read()
 
 setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    author='Bitcasa, Inc.',
+    name=bitcasa.__title__,
+    version=bitcasa.__version__,
+    author=bitcasa.__author__,
     author_email='api@bitcasa.com',
-    maintainer='Bitcasa, Inc.',
+    maintainer=bitcasa.__author__,
     maintainer_email='api@bitcasa.com',
     url='http://www.bitcasa.com/',
     description=('This is the Python SDK for the Bitcasa API.'),
     long_description=README,
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Environment :: Web Environment',
+                 'Natural Language :: English',
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.1',
-                 'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3',
                  'Topic :: Utilities'],
-    license='MIT',
+    license=bitcasa.__license__,
     keywords='bitcasa sdk',
-    packages=[PACKAGE_NAME, '{0}.tests'.format(PACKAGE_NAME)],
-    package_data={'': ['COPYING'], PACKAGE_NAME: ['*.ini']},
-    install_requires=['requests>=1.2.3','nose','nose-testconfig'],
+    packages=[bitcasa.__title__, '{0}.tests'.format(bitcasa.__title__)],
+    package_data={bitcasa.__title__: ['../testing.ini',
+                                 '../README.rst',
+                                 '../LICENSE']},
+    install_requires=['requests>=1.2.3', 'nose', 'nose-testconfig'],
     entry_points={},
-    test_suite='{0}.tests'.format(PACKAGE_NAME))
+    test_suite='{0}.tests'.format(bitcasa.__title__))
